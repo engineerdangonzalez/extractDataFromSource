244fa7937c122613464f22de4bd4518834b428ba
Daniel González <engineer.dangonzalez@gmail.com>
2020-12-20 13:19:59 -0600

    Coverage test added

--- extractDataFromSource.js
+++ extractDataFromSource.js
@@ -1,5 +1,14 @@
-function extractDataFromSource(source, params = {}) {
-    const newParams = { defaultKeys: ['id', 'userId'] };
+/**
+ *
+ * @param {array | object} source - Represents source data
+ * @param {object} param1 - It's the config object
+ */
+function extractDataFromSource(source, config = {}) {
+
+    if (!source && typeof source !== 'boolean') throw new Error('Invalid source data was provided');
+
+    const { requiredKeys } = config;
+    const newParams = { defaultKeys: ['id'].concat(requiredKeys) };
     let extractedData = null;
 
     // source contains a primitive value
--- extractDataFromSource.spec.js
+++ extractDataFromSource.spec.js
@@ -3,7 +3,10 @@ const extractDataFromSource = require('./extractDataFromSource.js');
 
 describe('Extracting data from source', () => {
     test('Return an array from an array source', () => {
-        const extractedData = extractDataFromSource(defaultSource);
+        const extractedData = extractDataFromSource(defaultSource, { requiredKeys: ['userId'] });
         expect(extractedData).toBeTruthy();
+    });
+    test('Return error when source is empty', () => {
+        expect(() => extractDataFromSource()).toThrow('Invalid source data was provided');
     });
 });
