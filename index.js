ea811d32cb31809d356e9e7c92c03ccd0c1a5a23
Daniel González <ing.daniel.go@gmail.com>
2021-01-10 15:32:21 -0600

    Using axios for testing code

--- index.js
+++ index.js
@@ -1,20 +1,10 @@
-const https = require('https');
+const axios = require('axios');
 const extractDataFromSource = require('./extractDataFromSource.js');
 
 
-https.get('https://rickandmortyapi.com/api/character/?page=19', (resp) => {
-
-    let defaultSource = '';
-
-    resp.on('data', (chunk) => {
-        defaultSource += chunk;
-    });
-
-    resp.on('end', () => {
-        const newData = JSON.parse(defaultSource);
-        extractDataFromSource(newData, { keys: ['episode', 'id', 'image', 'type'] });
-    });
-
-}).on('error', (err) => {
-    console.log(err);
+axios.get('https://rickandmortyapi.com/api/character/?page=19').then((response) => {
+    const newData = response.data;
+    console.log(extractDataFromSource(newData, { keys: ['episode', 'id', 'image', 'type', 'name'] }));
+}).catch(function (error) {
+    console.log(error);
 });
