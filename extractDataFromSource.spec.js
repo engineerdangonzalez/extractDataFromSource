5f3c3de0f63d10dd1820eb53bf149ae6c1a66c8a
Daniel González <ing.daniel.go@gmail.com>
2021-01-10 15:27:02 -0600

    Axios for get data for tests

--- extractDataFromSource.spec.js
+++ extractDataFromSource.spec.js
@@ -1,10 +1,14 @@
-const { defaultSource } = require('./source.js');
+const axios = require('axios');
 const extractDataFromSource = require('./extractDataFromSource.js');
 
 describe('Extracting data from source', () => {
     test('Return an array from an array source', () => {
-        const extractedData = extractDataFromSource(defaultSource, { requiredKeys: ['userId'] });
-        expect(extractedData).toBeTruthy();
+        axios.get('https://rickandmortyapi.com/api/character/?page=19').then((response) => {
+            const extractedData = extractDataFromSource(response.data, { keys: ['episode', 'id', 'image', 'type', 'name'] });
+            expect(extractedData).toBeTruthy();
+        }).catch(function (error) {
+            console.log(error);
+        });
     });
     test('Return error when source is empty', () => {
         expect(() => extractDataFromSource()).toThrow('Invalid source data was provided');
