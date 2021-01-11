6c2180688385d49c4c80c6078c324d58555429df
Daniel González <ing.daniel.go@gmail.com>
2021-01-11 13:55:40 -0600

    Example functionality code optimization

--- index.js
+++ index.js
@@ -2,9 +2,11 @@ const axios = require('axios');
 const extractDataFromSource = require('./extractDataFromSource.js');
 
 
-axios.get('https://rickandmortyapi.com/api/character/?page=19').then((response) => {
-    const newData = response.data;
-    console.log(extractDataFromSource(newData, { keys: ['episode', 'id', 'image', 'type', 'name'] }));
-}).catch(function (error) {
-    console.log(error);
-});
+const getFataFromSource = async (source = '') => {
+    return await axios.get(source);
+}
+
+(async () => {
+    const response = await getFataFromSource('https://rickandmortyapi.com/api/character/?page=19');
+    console.log(extractDataFromSource(response.data, { keys: ['episode', 'id', 'image', 'type', 'name'] }));
+})();
